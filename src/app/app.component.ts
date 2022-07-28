f4fa7eec55f90359ce83b190a91a6fc2b2a86286
Monika <91907563+mkford4@users.noreply.github.com>
2022-07-28 10:51:44 -0500

    Login update to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { MatDialog } from '@angular/material/dialog';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 
 @Component({
   selector: 'app-root',
@@ -16,6 +18,18 @@ export class AppComponent {
     this.dialog.open(UserRegistrationFormComponent, {
       //Assigning the dialog a width
       width: '280px'
+    });
+  }
+
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
+      width: '280px'
+    });
+  }
+
+  openMoviesDialog(): void {
+    this.dialog.open(MovieCardComponent, {
+      width: '500px'
     });
   }
 
